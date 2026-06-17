0988d22f0de4031e3a9a51acfaf7ed9ff0dc5695
cdric <cedvict@gmail.com>
2026-06-17 18:25:45 +0200

    Le router pour admin est près, prochaine mise à jour j'espere que je vais créer plusieur rooter

--- commands/admin/admin_handler.js
+++ commands/admin/admin_handler.js
@@ -4,6 +4,54 @@ if (typeof global.ROOT_ACTIVE === "undefined") {
     global.ROOT_ACTIVE = false;
     global.ROOT_USER = null;
 }
+// ======================
+// 🔧 NORMALISATION
+// ======================
+
+function normalize(jid = "") {
+
+    if (typeof jid !== "string") {
+        return "";
+    }
+
+    return jid
+        .replace(/@s\.whatsapp\.net/g, "")
+        .replace(/@lid/g, "")
+        .replace(/@c\.us/g, "")
+        .replace(/@g\.us/g, "")
+        .split(":")[0];
+}
+
+// ======================
+// 🔍 GROUP CHECK
+// ======================
+
+function isGroup(jid = "") {
+    return jid.endsWith("@g.us");
+}
+
+// ======================
+// 👤 EXTRAIRE VRAI USER
+// ======================
+
+function getRealSender(message = {}) {
+
+    const remoteJid =
+        message?.key?.remoteJid || "";
+
+    // 👥 groupe
+    if (isGroup(remoteJid)) {
+
+        return (
+            message?.key?.participant ||
+            ""
+        );
+    }
+
+    // 📩 privé
+    return remoteJid;
+}
+        
 
 module.exports = async function adminHandler({
     sock,
@@ -13,6 +61,9 @@ module.exports = async function adminHandler({
 }) {
 
     const sender = message.key.remoteJid;
+    const realSender = getRealSender(message);
+
+    const senderNorm = normalize(realSender);
 
     // ======================
     // 👑 ROOT COMMAND EXCEPTION
@@ -41,7 +92,7 @@ module.exports = async function adminHandler({
     // 🔐 CHECK ROOT STATE
     // ======================
 
-    if (!global.ROOT_ACTIVE || global.ROOT_USER !== sender) {
+    if (!global.ROOT_ACTIVE || global.ROOT_USER !== senderNorm) {
 
         return await sock.sendMessage(sender, {
             text:
